e67aa38ebb220892279f3688116204926a279b00
aitoehigie <aitoehigie@gmail.com>
2014-06-18 04:45:42 +0100

    Complete rewrite with Requests

--- sms.py
+++ sms.py
@@ -23,33 +23,29 @@ supported_networks = [234708,234802,234808,234812,234705,234805,234811,234807,23
 #****************************************************************
 
 def login(owneremail=owneremail, subacct=subacct, subacctpwd=subacctpwd):
-	params = urllib.urlencode(dict(cmd="login", owneremail=owneremail, subacct=subacct, subacctpwd=subacctpwd))
-	response = urllib2.urlopen(url, params)
-	for item in response.readlines():
-		sessionID = item[4:]
-		return sessionID
-	response.close()
-sessionID = login()
+	payload = dict(cmd="login", owneremail=owneremail, subacct=subacct, subacctpwd=subacctpwd)
+	global sessionID
+	sessionID = requests.post(url, data=payload).text[4:]
+	return sessionID
 
 def getacctbalance(cmd="querybalance"):
-	params = urllib.urlencode(dict(cmd=cmd, sessionid=sessionID))
-	response = urllib.urlopen(url, params)
-	for item in response.readlines():
-		return item[4:]
-	response.close()
+	payload = dict(cmd=cmd, sessionid=sessionID)
+	response = requests.post(url, data=payload)
+	return response.text[4:]
 
 def sendmessage(message, sendto, sessionid=sessionID, sender=sender):
 	if len(str(sendto)) != 13:
 		return "That is not a valid Nigerian GSM number!"
 	elif len(str(sendto)) == 13 and str(sendto)[0:6] in str(supported_networks):
-		params = urllib.urlencode(dict(sendto=str(sendto), cmd="sendmsg", sessionid=sessionID, message=message, sender=sender))
-		response = urllib2.urlopen(url, params)
-		for item in response.readlines():
-			return item[4:]
-		response.close()
+		payload = dict(sendto=str(sendto), cmd="sendmsg", sessionid=sessionID, message=message, sender=sender)
+		response = requests.post(url, data=payload).text[4:]
+		return response
 	else:
 		return "You are trying to send a message to an unsupported network!"
 
+
+if __name__ == "__main__":
+	sessionID = login(owneremail=owneremail, subacct=subacct, subacctpwd=subacctpwd)
 
 
 
