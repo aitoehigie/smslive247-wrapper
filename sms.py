007808507a9b2da4d89e3c27096fa6a3b3174469
aitoehigie <aitoehigie@gmail.com>
2014-06-21 11:05:19 +0100

    Refactored code to become PEP8 compliant

--- sms.py
+++ sms.py
@@ -1,51 +1,59 @@
 #!/usr/bin/env python
 
-#**************************************************************************************
+# *****************************************************************************
 """
-description= Python HTTP API wrapper for smslive247.com
-author=Pystar
-author_email=aitoehigie@gmail.com.
-Date=2013
+description = Python HTTP API wrapper for smslive247.com
+author = Pystar
+author_email = aitoehigie@gmail.com.
+Date = 2013
 Revised: 18/06/2014
 """
 
-import requests
-
-#***********************global-settings*************************
+# ***********************global-settings***************************************
 global url, owneremail, subacct, subacctpwd, sender, number_range, sessionID
 url = "http://www.smslive247.com/http/index.aspx?"
-owneremail="your email address registered on smslive247.com"
-subacct="your smslive247.com subacct username"
-subacctpwd="your smslive247.com subacct password"
+owneremail = "your email address registered on smslive247.com"
+subacct = "your smslive247.com subacct username"
+subacctpwd = "your smslive247.com subacct password"
 sender = "display name of the sender"
-supported_networks = [234708,234802,234808,234812,234705,234805,234811,234807,234815,234703,234706,234803,234806,234810,
-234813,234816,234809,234817,234818]
-#****************************************************************
+supported_networks = [234708, 234802, 234808, 234812, 234705,
+                      234805, 234811, 234807, 234815, 234703, 234706, 234803,
+                      234806, 234810, 234813, 234816, 234809, 234817, 234818]
+# **************************************************************************
+
+import requests
+
 
 def login(owneremail=owneremail, subacct=subacct, subacctpwd=subacctpwd):
-	payload = dict(cmd="login", owneremail=owneremail, subacct=subacct, subacctpwd=subacctpwd)
-	global sessionID
-	sessionID = requests.post(url, data=payload).text[4:]
-	return sessionID
+    payload = dict(cmd="login", owneremail=owneremail, subacct=subacct,
+                   subacctpwd=subacctpwd)
+    global sessionID
+    sessionID = requests.post(url, data=payload).text[4:]
+    return sessionID
+
+login(owneremail=owneremail, subacct=subacct, subacctpwd=subacctpwd)
+
 
 def getacctbalance(cmd="querybalance"):
-	payload = dict(cmd=cmd, sessionid=sessionID)
-	response = requests.post(url, data=payload)
-	return response.text[4:]
+    """Account balance query function."""
+    payload = dict(cmd=cmd, sessionid=sessionID)
+    response = requests.post(url, data=payload)
+    return response.text[4:]
+
 
 def sendmessage(message, sendto, sessionid=sessionID, sender=sender):
-	if len(str(sendto)) != 13:
-		return "That is not a valid Nigerian GSM number!"
-	elif len(str(sendto)) == 13 and str(sendto)[0:6] in str(supported_networks):
-		payload = dict(sendto=str(sendto), cmd="sendmsg", sessionid=sessionID, message=message, sender=sender)
-		response = requests.post(url, data=payload).text[4:]
-		return response
-	else:
-		return "You are trying to send a message to an unsupported network!"
+    """SMS sending function."""
+    if len(str(sendto)) != 13:
+        return "That is not a valid Nigerian GSM number!"
+    elif len(str(sendto)) == 13 and str(sendto)[0:6] in str(supported_networks):
+        payload = dict(sendto=str(sendto), cmd="sendmsg", sessionid=sessionID,
+                       message=message, sender=sender)
+        response = requests.post(url, data=payload).text[4:]
+        return response
+    else:
+        return "You are trying to send a message to an unsupported network!"
 
 
 if __name__ == "__main__":
-	sessionID = login(owneremail=owneremail, subacct=subacct, subacctpwd=subacctpwd)
-
-
-
+    sessionID = login(owneremail=owneremail, subacct=subacct,
+                      subacctpwd=subacctpwd)
